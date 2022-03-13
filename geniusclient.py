7cbd32ec7732d4381e3b5814cb7e02598c20f9e4
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2022-03-13 03:19:48 -0700

    Update geniusclient.py

--- geniusclient.py
+++ geniusclient.py
@@ -1,30 +1,40 @@
+# Implemented Lyrics Web Scraper by Ivan Delgado
+
 # Library to access content on Genius.com
 import lyricsgenius as lg
 
 # Search and manipulate strings
 import re
 
+# Used for creating file if it does not exist
+from pathlib import Path
+
 # We retrieve data from the Genius API using the LyricsGenius python module
-api = lg.Genius("my_access_token_here",
-		skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
+api = lg.Genius('mG0s2LL-EdEB-YDZvpf3bE2WjXuDdple7riOw5cEn5T_zM8RsbXGPL5LyfgWwnMw',
+                skip_non_songs=True, verbose=False, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
 
-# Write to file
-file = open("lyrics.txt", "w")
+def lyrics_scraper(genre):
 
-# Getting lyrics of songs by genre
-page = 1
-count = 0 
-while page <= 2:
-# Try Except statements to handle when a timeout occurs
-    try:
-        results = api.tag('country', page=page)
-        for hit in results['hits']:
-            song_lyrics = api.lyrics(song_url=hit['url'], )
-            file.write(song_lyrics)
-        page = results['next_page']      
-    except:
-        pass
+                # Create file if it does not exist using the touch() method
+    fle = Path("lyrics.txt")
+    fle.touch(exist_ok=True)
+    f = open(fle)
 
-file.close()
+    # Write to file
+    file = open("lyrics.txt", 'w')
 
+    # Getting lyrics of songs by genre
+    page = 2
+    while page < 4:
+        # Try Except statements to handle when a timeout occurs
+        try:
+            results = api.tag(genre, page=page)
+            for hit in results['hits']:
+                song_lyrics = api.lyrics(song_url=hit['url'], )
+                file.write(song_lyrics)
+                file.write('\n\n')
+            page = results['next_page']
+        except:
+            pass
 
+    file.close()
